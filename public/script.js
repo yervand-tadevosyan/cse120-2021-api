14cb1ee865814ada5e040f8d0948357bb44b3387
yervand-tadevosyan <81684581+yervand-tadevosyan@users.noreply.github.com>
2021-04-09 13:30:05 +0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -196,6 +196,34 @@ if(validateFormData() == false){
   }
 }
 
+function updateData(e) {
+e.preventDefault();
+if(validateFormData() == false){
+  return;
+}else{
+  console.log(myBook);
+
+    $.ajax({
+    type: 'POST',
+    url: "https://cse120-2021-api-yervand.herokuapp.com/data/update",
+    data: myBook,
+    cache: false,
+    dataType : 'json',
+    success: function (data) {
+      console.log("success");
+    },
+    error: function (xhr) {
+      console.error("Error in post", xhr);
+    },
+    complete: function () {
+      console.log("Complete");  
+    }
+  });
+  }
+}
+
+
+
 
 function loadExistingData() {
     myTennisData = [];
@@ -406,6 +434,7 @@ function toggleOtherData() {
     otherData.style.display = "block";
   }
 }
+
 
 
 
