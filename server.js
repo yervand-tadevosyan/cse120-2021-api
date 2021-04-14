8909d23dae4dcca7d64ca5f241766b970ccb21e8
yervand-tadevosyan <81684581+yervand-tadevosyan@users.noreply.github.com>
2021-04-14 11:38:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/data/update', function (req, res) {
     let id = req.body.id;
     let newValue = req.body.value;
     const query = { "_id": ObjectId(id)};
-    client.db('cse120-2021-db').collection('books').update(query,{$set: req.body})
+    client.db('cse120-2021-db').collection('books').updateOne(query,{$set: req.body})
       .then(result => {
         console.log(result)
         res.send({"message":"Updated"});
